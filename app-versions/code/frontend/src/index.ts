a128025db662ad5717e7e96c5f20b469b89dab55
Akhil Sharma (155825) <AKHILSHA@amdocs.com>
2026-09-11 08:01:40 +0530

    added dev and prod yaml

--- app-versions/code/frontend/src/index.ts
+++ app-versions/code/frontend/src/index.ts
@@ -29,7 +29,7 @@ async function startServer(): Promise<void> {
     app.use(express.static(join(__dirname, 'public')));
 
     // Initialize services
-    console.log('Initializing services-- akhil');
+    console.log('Initializing services-- akhil - akhil');
 
     queueService = new QueueService({
       host: REDIS_HOST,
